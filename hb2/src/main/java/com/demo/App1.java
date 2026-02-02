aec9de06479ce8db038d95b3d925a710fbb0cc52
sopan pramod patil <143089038+SOPANPATIL09@users.noreply.github.com>
2026-02-02 16:49:59 +0530

    find third largest salary employee from employee table using hcql

--- hb2/src/main/java/com/demo/App1.java
+++ hb2/src/main/java/com/demo/App1.java
@@ -1,6 +1,7 @@
 package com.demo;
 
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.hibernate.query.Query;
@@ -17,31 +18,35 @@ import org.hibernate.Transaction;
 
 public class App1 {
 	public static void main(String[] args) {
-		
+		 
 		Configuration con=new Configuration().configure();
 	 	
 	     SessionFactory sf=con.buildSessionFactory();
 	     
 	    Session session =sf.openSession();
-	     Transaction tx=session.beginTransaction();
-	     
-        
-	        List<Employee> list=session.createCriteria(Employee.class)
-	        .add(Restrictions.eq("salary",      session.createCriteria(Employee.class)
-	   	         .add(Restrictions.lt("salary",   session.createCriteria(Employee.class)
-	   	 	         .setProjection(Projections.max("salary"))
-	   	 	         .uniqueResult()))
-	   	 	         .setProjection(Projections.max("salary"))
-	   	 	         .uniqueResult()))
-	                 .list();
-	                
-	                   for(Employee e:list)
-	                	   System.out.println(e);
-	  //session.update(e);
-	  tx.commit();
+	    Transaction tx=session.beginTransaction();
+	    
+		   List<Employee> list=session.createCriteria(Employee.class)
+	       .add(Restrictions.eq("salary",  session.createCriteria(Employee.class)
+		   .add(Restrictions.lt("salary",  session.createCriteria(Employee.class)
+		  .add(Restrictions.lt("salary",session.createCriteria(Employee.class)
+		    .setProjection(Projections.max("salary"))
+		    .uniqueResult()
+		      ))
+		     .setProjection(Projections.max("salary"))
+		     .uniqueResult()
+		      ))
+		     .setProjection(Projections.max("salary"))
+		     .uniqueResult()
+		      ))
+	         .list();
+		   for(Employee e:list)
+			   System.out.println(e);
+  // session.save();
+	  tx.commit(); 
 	  session.close();
 	}
-		
+
 	        
 	                
 }   
